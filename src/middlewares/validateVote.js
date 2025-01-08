743b98f421d98d3ea28720ce6721088b66a18113
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2025-01-08 22:47:59 +0530

    fix: update validateVote

--- src/middlewares/validateVote.js
+++ src/middlewares/validateVote.js
@@ -1,10 +1,12 @@
 import Vote from "../models/voteModel.js";
 
 export const validateVote = async (req, res, next) => {
-  const { userId } = req.user;
+  const { walletAddress } = req.user;
+  console.log(walletAddress)
+  console.log(req.user.walletAddress)
 
   // Check if the user has already voted
-  const existingVote = await Vote.findOne({ userId });
+  const existingVote = await Vote.findOne({ walletAddress });
   if (existingVote) {
     return res.status(400).json({ error: "You have already voted." });
   }
